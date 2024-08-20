b2c97b307e338c2b7afd94945840f18e5edc2243
DimasAditya2 <esadimasadityaa@gmail.com>
2024-08-20 16:51:11 +0700

    feat: login

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,71 @@
+import { signIn } from "@/lib/firebase/service";
+import { compare } from "bcrypt";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      type: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const user: any = await signIn(email);
+        if (user) {
+          const isValidPassword = await compare(password, user.password);
+          if (isValidPassword) {
+            return user;
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, profile, user, account }: any) {
+      if (account?.provider == "credentials") {
+        token.email = user.email;
+        token.fullName = user.fullName;
+        token.password = user.password;
+        token.phoneNumber = user.phoneNumber;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      if ("email" in token) {
+        session.user.email = token.email;
+      }
+      if ("fullName" in token) {
+        session.user.fullName = token.fullName;
+      }
+      if ("phoneNumber" in token) {
+        session.user.phoneNumber = token.phoneNumber;
+      }
+      if ("password" in token) {
+        session.user.password = token.password;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
+export default NextAuth(authOptions);
--- src/views/auth/Logins/index.tsx
+++ src/views/auth/Logins/index.tsx
@@ -1,19 +1,48 @@
 import Link from "next/link";
 import styles from "./Login.module.scss";
 import { FaEye } from "react-icons/fa";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
 const LoginView = () => {
-    const handlerLogin = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const form = e.target as HTMLFormElement
-        const data = {
-            email: form.email.value,
-            password: form.password.value
-        }
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const { push, query } = useRouter();
+
+  const callbackUrl: any = query.callbackUrl || "/";
+
+  const handlerLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target as HTMLFormElement;
+    try {
+      const res = await signIn("credentials", {
+        redirect: false,
+        email: form.email.value,
+        password: form.password.value,
+        callbackUrl: Array.isArray(callbackUrl) ? callbackUrl[0] : callbackUrl,
+      });
+
+      console.log(res?.ok)
+
+      if (!res?.error) {
+        setLoading(false);
+        form.reset();
+        push(callbackUrl);
+      } else {
+        setLoading(false);
+        setError("Wrong email or password");
+      }
+    } catch (error: any) {
+      setLoading(false);
+      setError(error);
+      console.log(error);
     }
+  };
   return (
     <div className={styles.login}>
       <h1 className={styles.login__title}>Login</h1>
+      {error && <p className={styles.login__error}>{error}</p>}
       <div className={styles.login__form}>
         <form onSubmit={handlerLogin}>
           <div className={styles.login__input}>
@@ -25,7 +54,9 @@ const LoginView = () => {
               className={styles.login__input__field}
             />
           </div>
-          <div className={`${styles.login__input} ${styles.login__input__password}`}>
+          <div
+            className={`${styles.login__input} ${styles.login__input__password}`}
+          >
             <input
               type="password"
               name="password"
@@ -33,14 +64,22 @@ const LoginView = () => {
               placeholder="Password"
               className={styles.login__input__field}
             />
-            <div className={styles.login__input__password__icon}><FaEye /></div>
+            <div className={styles.login__input__password__icon}>
+              <FaEye />
+            </div>
           </div>
           <div className={styles.login__nav__login}>
             <p>
-              Dont have an account ? <Link href="/auth/register" className={styles.login__nav__login__link}>Sign up</Link>
+              Dont have an account ?{" "}
+              <Link
+                href="/auth/register"
+                className={styles.login__nav__login__link}
+              >
+                Sign up
+              </Link>
             </p>
-            <button className={styles.login__nav__login__button}>
-              Login
+            <button type="submit" className={styles.login__nav__login__button}>
+              {loading ? "Loading..." : "Login"}
             </button>
           </div>
         </form>
